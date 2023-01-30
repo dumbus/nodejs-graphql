0f08a8a460fb658b497fea17ff9288e03ce009ae
dumbus <dumbusx@gmail.com>
2023-01-30 03:21:48 +0300

    feat: add graphql requests to get all entities

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,8 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { GraphQLUser, GraphQLProfile, GraphQLPost, GraphQLMemberType } from './types';
 import { graphqlBodySchema } from './schema';
+
+import { GraphQLObjectType, GraphQLList, GraphQLSchema, graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -11,7 +14,40 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const RootQuery = new GraphQLObjectType({
+        name: 'RootQuery',
+        fields: {
+          users: {
+            type: new GraphQLList(GraphQLUser),
+            resolve: () => fastify.db.users.findMany()
+          },
+
+          profiles: {
+            type: new GraphQLList(GraphQLProfile),
+            resolve: () => fastify.db.profiles.findMany()
+          },
+
+          posts: {
+            type: new GraphQLList(GraphQLPost),
+            resolve: () => fastify.db.posts.findMany()
+          },
+
+          memberTypes: {
+            type: new GraphQLList(GraphQLMemberType),
+            resolve: () => fastify.db.memberTypes.findMany()
+          }
+        }
+      });
+
+      const schema = new GraphQLSchema({
+        query: RootQuery
+      });
+
+      const result = await graphql({ schema: schema, source: request.body.query! });
+
+      return result;
+    }
   );
 };
 
---
+++ src/routes/graphql/types.ts
@@ -0,0 +1,47 @@
+import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } from 'graphql';
+
+const GraphQLUser = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLID },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString }
+    })
+});
+
+const GraphQLProfile = new GraphQLObjectType({
+    name: 'Profile',
+    fields: () => ({
+        id: { type: GraphQLID },
+        avatar: { type: GraphQLString },
+        sex: { type: GraphQLString },
+        birthday: { type: GraphQLInt },
+        country: { type: GraphQLString },
+        street: { type: GraphQLString },
+        city: { type: GraphQLString },
+        userId: { type: GraphQLID },
+        memberTypeId: { type: GraphQLString }
+    })
+});
+
+const GraphQLPost = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        content: { type: GraphQLString },
+        userId: { type: GraphQLID }
+    })
+});
+
+const GraphQLMemberType = new GraphQLObjectType({
+    name: 'MemberType',
+    fields: () => ({
+        id: { type: GraphQLID },
+        discount: { type: GraphQLInt },
+        monthPostsLimit: { type: GraphQLInt }
+    })
+});
+
+export { GraphQLUser, GraphQLProfile, GraphQLPost, GraphQLMemberType };
